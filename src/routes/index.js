b348905720ad7d3bffa11dc7b3565a2e63cb8a70
frederico <fredyramos0707@gmail.com>
2024-01-28 13:44:15 -0300

    116 - Pagina novo chamado

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import Dashboard from '../pages/dashboard';
 import Private from './Private';
 import Profile from '../pages/Profile';
 import Customers from '../pages/costumers';
+import New from '../pages/New'
 
 function RoutesApp(){
   return(
@@ -16,6 +17,7 @@ function RoutesApp(){
       <Route path="/dashboard" element={ <Private><Dashboard/> </Private>} />
       <Route path="/profile" element={<Private><Profile/></Private>}/>
       <Route path="/customers" element={<Private><Customers/></Private>}/>
+      <Route path="/new" element={<Private><New/></Private>} />
     </Routes>
   )
 }
