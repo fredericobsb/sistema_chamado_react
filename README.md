538497d7e870dbb5fb5ea3ba14e0bff7e960ad6e
frederico <fredyramos0707@gmail.com>
2024-01-27 23:46:08 -0300

    100 - configurando firebase

---
+++ README.md
@@ -0,0 +1,5 @@
+----- 100 - CONFIGURANDO O FIREBASE ----------------
+
+1 - crie o banco de dados no firebase
+2 - clique no simbolo "web" para adicionar o banco ao projeto
+3 - instale o firebase no projeto com o comando: npm install firebase
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,21 @@
+import{InitializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+import {getStorage} from 'firebase/storage';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUuiA9Cw0dxXbCkPjNGdUE3_JGHHKzfuI",
+    authDomain: "chamados-66478.firebaseapp.com",
+    projectId: "chamados-66478",
+    storageBucket: "chamados-66478.appspot.com",
+    messagingSenderId: "1005778485544",
+    appId: "1:1005778485544:web:87ed2830094f40e0e3d01d"
+  };
+
+  const firebaseApp = InitializeApp(firebaseConfig);
+  const auth = getAuth(firebaseApp);
+  const db = getFirestore(firebaseApp);
+  const storage = getStorage(firebaseApp);
+
+  export {firebaseApp, auth, db, storage};
