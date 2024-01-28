76e3148c47717bdb7bcf77188b80ada34db77480
frederico <fredyramos0707@gmail.com>
2024-01-27 23:07:28 -0300

    99 - pagina de cadastro ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
+
 import Signin from '../pages/signin';
 import Signup from '../pages/signup';
+import Dashboard from '../pages/dashboard';
 
 function RoutesApp(){
-    return(
-       <Routes>
-        <Route path='/' element={<Signin/>}/>
-        <Route path='/register' element={<Signup/>}/>
-       </Routes>
-    );
+  return(
+    <Routes>
+      <Route path="/" element={ <Signin/> } />
+      <Route path="/register" element={ <Signup/> } />
+
+      <Route path="/dashboard" element={ <Dashboard/> } />
+    </Routes>
+  )
 }
+
 export default RoutesApp;
