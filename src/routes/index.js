1f2c813442c254eb473f8c34db43309092803627
frederico <fredyramos0707@gmail.com>
2024-01-28 12:49:25 -0300

    112 - Pagina clientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Signup from '../pages/signup';
 import Dashboard from '../pages/dashboard';
 import Private from './Private';
 import Profile from '../pages/Profile';
+import Customers from '../pages/costumers';
 
 function RoutesApp(){
   return(
@@ -14,6 +15,7 @@ function RoutesApp(){
 
       <Route path="/dashboard" element={ <Private><Dashboard/> </Private>} />
       <Route path="/profile" element={<Private><Profile/></Private>}/>
+      <Route path="/customers" element={<Private><Customers/></Private>}/>
     </Routes>
   )
 }
